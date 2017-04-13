f5216c46eb05ba13241edf77ab16cdedb5232ed2
liuboyce <liubaiyu_sunshine@163.com>
2017-04-13 20:54:32 +0800

    Initial commit

---
+++ src/cn/itcast/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.itcast.hello;
+
+/**
+ * Created by liubaiyu on 2017/4/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.print("second");
+
+    }
+}
